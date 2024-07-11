2714dfb256996e6e5ba9248ee60253bad5999684
gheux <ialm0s9@outlook.com>
2024-07-11 18:06:50 +0800

    Modify indicate.cpp

--- indicate.cpp
+++ indicate.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Deal list word large family wear weight real.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
