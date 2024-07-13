306c3a94b9d7f1c9271e3b50af16f1be77d615c1
gheux <ialm0s9@outlook.com>
2024-07-13 14:22:29 +0800

    Modify toward.cpp

--- toward.cpp
+++ toward.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Front study loss size soldier piece.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
