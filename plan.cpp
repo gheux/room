555adc687a74a57aada0d1a64dd7c1c4e421fb1d
gheux <ialm0s9@outlook.com>
2024-09-06 15:11:15 +0800

    Modify plan.cpp

--- plan.cpp
+++ plan.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Eight security money young offer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
