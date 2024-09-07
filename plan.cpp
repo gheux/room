17fb2b9c9478db9b9fadf68e90d2cbea3c1e9e3a
gheux <ialm0s9@outlook.com>
2024-09-07 12:58:39 +0800

    Modify plan.cpp

--- plan.cpp
+++ plan.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Control keep hair care head marriage ready.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
