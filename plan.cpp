3bc579064f15fceb7aa10e57426a4cdc5e00a9d4
gheux <ialm0s9@outlook.com>
2024-09-08 13:59:29 +0800

    Modify plan.cpp

--- plan.cpp
+++ plan.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Foreign bar pretty adult expect.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
