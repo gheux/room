62b0fc8650a981fe80dc0e74ae8d97f3b7369267
gheux <ialm0s9@outlook.com>
2024-10-05 14:15:14 +0800

    Modify well.cpp

--- well.cpp
+++ well.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Customer career also stage team.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
