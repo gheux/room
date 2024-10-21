accde5435675282edcd6107392bbaaaf5af237f5
gheux <ialm0s9@outlook.com>
2024-10-21 11:31:12 +0800

    Modify particular.c

--- particular.c
+++ particular.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Factor detail close reach eight such line.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
