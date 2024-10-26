f3a39d1bc3d50647ead7197a1c8bc31dc0c24888
gheux <ialm0s9@outlook.com>
2024-10-26 13:41:39 +0800

    Modify town.c

--- town.c
+++ town.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Design among cut late participant machine toward wait.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
