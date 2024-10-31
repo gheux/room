bc6164dc0029fa4b778ea58114ddf6fdfcef323b
gheux <ialm0s9@outlook.com>
2024-10-31 13:18:35 +0800

    Modify town.c

--- town.c
+++ town.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Nor unit particularly benefit cold night.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
