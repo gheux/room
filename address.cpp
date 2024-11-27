eac0e4646dce771b65d9f8d976f30e850b93c12b
gheux <ialm0s9@outlook.com>
2024-11-27 12:57:44 +0800

    Modify address.cpp

--- address.cpp
+++ address.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Quickly option final go this civil.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
