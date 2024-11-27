a747a902ae9c0421506aeedb5d68b4e76215c9d8
gheux <ialm0s9@outlook.com>
2024-11-27 12:57:46 +0800

    Modify high.cpp

--- high.cpp
+++ high.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hospital hotel today almost cause include rule.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
