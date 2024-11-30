6bfe2d3df2eff215b60221b6af192ceb66a9c589
gheux <ialm0s9@outlook.com>
2024-11-30 23:43:05 +0800

    Modify leave.go

--- leave.go
+++ leave.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Theory specific offer somebody friend.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
