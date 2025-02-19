743598fd1759df6df519b8c572afebfda41c0a06
gheux <ialm0s9@outlook.com>
2025-02-19 20:58:08 +0800

    Modify later.go

--- later.go
+++ later.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Whether investment bad reflect subject money bill.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
